b7d1f59031f9b91c8106e10b7d9318de6e59efcd
sunil singh <suniljersey201@gmail.com>
2022-02-09 16:59:08 +0530

    Updates

---
+++ precache-manifest.6953aea0bb947d914bfcd37411de619b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4dcf5b29d67c795755a77317c19023",
+    "url": "/TextUtlis-React/index.html"
+  },
+  {
+    "revision": "a7a58444f2780735ae03",
+    "url": "/TextUtlis-React/static/css/main.2c7a7338.chunk.css"
+  },
+  {
+    "revision": "f90d664046b8500c42cd",
+    "url": "/TextUtlis-React/static/js/2.4483066c.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/TextUtlis-React/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "a7a58444f2780735ae03",
+    "url": "/TextUtlis-React/static/js/main.a2ca292c.chunk.js"
+  },
+  {
+    "revision": "f17e8ecce841269ee4f8",
+    "url": "/TextUtlis-React/static/js/runtime~main.3aef88e4.js"
+  }
+]);
